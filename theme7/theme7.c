0771bc073d7eeeaae00ebbb4784df67b9f6c86f0
Aurélie saulquin <aurelie.saulq@gmail.com>
2024-04-10 10:46:58 +0200

    ajout theme7

--- theme7/theme7.c
+++ theme7/theme7.c
@@ -15,7 +15,9 @@ int init(pile_t*);       /* initialise la pile */
 int push(pile_t*, int);  /* empile une valeur */
 int pop(pile_t*, int*); /* dépile une valeur */
 int destroy(pile_t*);    /* désalloue toute la pile */
-
+void display(pile_t*);  /* Affiche toute la pile */
+int insert(pile_t*, int, int); /* insert un element a une certaine position */
+int premove(pile_t*, int*, int); /* retire un element a une certaine position */
 
 int is_empty(pile_t pile){
   return pile==PILE_VIDE;
@@ -32,51 +34,117 @@ int init(pile_t* pile){
 }
 
 int push(pile_t* pile, int val){
-  // TODO
+  
+  pile_t next;
+
+  next = malloc(sizeof(pile_t));
+
+  if(next==NULL)
+    return -1;
+
+  next->val = val;
+  next->next = *pile;
+
+  *pile = next;
+  return 0;
 }
 
 int pop(pile_t* pile, int* val){
-  // TODO
+  
+  pile_t top;
+
+  if(is_empty(*pile))
+    return -1;
+
+  top = *pile;
+
+  *pile = top->next;
+
+  *val = top->val;
+
+  free(top);
+
+  return 0;
+
 }
 
 int destroy(pile_t* pile){
-  // TODO  
-}
-
-
-int main(){
-
-  pile_t pile;
-
-  init(&pile);
-
-  printf("%d\n", is_empty(pile));
   
-  push(&pile, 1);
-  
-  printf("%d\n", is_empty(pile));
-  
-  push(&pile, 2);
+  int buff;
 
-  int val = 0;
-  
-  pop(&pile, &val);
-  printf("%d\n", val);
-
-  pop(&pile, &val);
-  printf("%d\n", val);
-
-  printf("%d\n", is_empty(pile));
-  
-  push(&pile, 1);
-  push(&pile, 2);
-
-  printf("%d\n", is_empty(pile));
-
-  destroy(&pile);
-  printf("%d\n", is_empty(pile));
+  while(!is_empty(*pile)){
+    if(pop(pile, &buff)!=0)
+      return -1;
+  }
 
   return 0;
 }
 
+void display(pile_t* pile) {
+  
+  if(is_empty(*pile)){
+    printf("Empty stack\n");
+    return;
+  }
+
+  pile_t elem = *pile;
+
+  while(elem != NULL){
+    printf("%d", elem->val);
+    if(elem->next != NULL)
+      printf(" -> ");
+    elem = elem->next;
+  }
+
+  printf("\n");
+}
+
+int insert(pile_t* pile, int val, int pos){
+  if(pos==0)
+    return push(pile, val);
+  else
+    return insert(&((*pile)->next), val, pos-1);
+}
+
+int premove(pile_t* pile, int* val, int pos){
+  if(pos==0)
+    return pop(pile, val);
+  else
+    return premove(&((*pile)->next), val, pos-1);
+}
+
+
+int main(){
+   
+  pile_t pile;
+
+  init(&pile);
+
+  display(&pile);
+
+  printf("%d\n", push(&pile, 3));
+  printf("%d\n", push(&pile, 1));
+
+  int buff;
+
+  display(&pile);
+
+  insert(&pile, 2, 1);
+
+  display(&pile);
+
+  pop(&pile, &buff);
+  printf("buff = %d\n", buff);
+
+  display(&pile);
+
+  premove(&pile, &buff, 1);
+  printf("buff = %d\n", buff);
+
+  display(&pile);
+
+  destroy(&pile);
+
+  return 0;
+}
 
